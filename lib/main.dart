a0071261e8a0ac89ccf7bda823c191281a6a2447
Anika Tabassum <anikatabassum2738@gmail.com>
2026-04-08 18:33:55 +0600

    fixed path

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'widgets/header.dart';
 import 'widgets/balance_card.dart';
+import 'widgets/action_button.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,10 +19,40 @@ class MyApp extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.all(20.0),
             child: Column(
-              children: const [
-                Header(),
-                SizedBox(height: 25),
-                BalanceCard(),
+              children: [
+                const Header(),
+                const SizedBox(height: 25),
+                const BalanceCard(),
+
+                const SizedBox(height: 15),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: ActionButton(
+                        icon: Icons.swap_vert_rounded,
+                        label: "Transfer",
+                      ),
+                    ),
+
+                    const SizedBox(width: 15),
+                    Expanded(
+                      child: ActionButton(
+                        icon: Icons.account_balance_wallet_outlined,
+                        label: "Pay Bills",
+                      ),
+                    ),
+                    const SizedBox(width: 15),
+
+                    Expanded(
+                      child: ActionButton(
+                        icon: Icons.link_rounded,
+                        label: "Invest",
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
           ),
---
+++ lib/widgets/action_button.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class ActionButton extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const ActionButton({super.key, required this.icon, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 10,
+            offset: const Offset(0, 5),
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(12),
+            decoration: const BoxDecoration(
+              color: Color(0xFFE8EAF6),
+              shape: BoxShape.circle,
+            ),
+            child: Icon(icon, color: const Color(0xFF5D5FEF), size: 24),
+          ),
+          const SizedBox(height: 12),
+          Text(
+            label,
+            style: const TextStyle(
+              fontSize: 13,
+              fontWeight: FontWeight.w500,
+              color: Color(0xFF4A5568),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
